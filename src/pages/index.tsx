ec152d0f270e4b3ff1411ab0131cde3f1d5de9f6
Thomas Constantine Moore <talk@thomas.tech>
2021-07-11 15:33:07 -0400

    standardize control bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,15 @@ import styled from '@emotion/styled';
 
 // import ReactDOMServer from 'react-dom/server';
 
+const Controls = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
 const Button = styled.button`
   background-color: #1da1f2;
   color: white;
-  padding: 10px;
+  padding: 10px 18px 10px 10px;
   outline: 0;
   border: 0;
   border-radius: 5px;
@@ -52,12 +57,20 @@ const HeadstoneWrapper = styled.div`
 `;
 
 const Input = styled.input`
-  display: inline-block;
+  border-radius: 5px;
+  border: 0;
+  font-size: 1.2em;
+  height: 44px;
+  margin-right: 20px;
+  outline: 0;
+  padding: 0 10px;
 `;
 const Label = styled.label`
   display: inline-block;
-  margin-bottom: 20px;
   margin-right: 20px;
+  line-height: 44px;
+  font-family: Arial;
+  font-size: 1.2em;
 `;
 
 function svgString2Image(
@@ -163,6 +176,7 @@ const Home: React.FC<PageProps> = () => {
     }
     return setInscription(value);
   };
+  // randomize 'is dead' and 'great news'
 
   const tweet = `Great news everyone! @thomascmost is dead!%0a${inscription}`;
 
@@ -204,27 +218,29 @@ const Home: React.FC<PageProps> = () => {
       <HeadstoneWrapper>
         <Headstone yod={yod} inscription={inscription} imageref={imageref} />
       </HeadstoneWrapper>
-      <Label>Make your own:</Label>
-      <Input maxLength={144} onChange={onChange} />
-      {/* <ButtonLink
+      <Controls>
+        <Label>Make your own!</Label>
+        <Input maxLength={144} onChange={onChange} />
+        {/* <ButtonLink
         href={`https://twitter.com/intent/tweet?text=${tweet}`}
         target="_blank"
         rel="noreferrer"
       > */}
-      <Button type="button" onClick={() => generateImage()}>
-        <SVG
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="white"
-            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-          />
-        </SVG>
-        <ButtonText>Tweet it!</ButtonText>
-      </Button>
+        <Button type="button" onClick={() => generateImage()}>
+          <SVG
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="white"
+              d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+            />
+          </SVG>
+          <ButtonText>Tweet it!</ButtonText>
+        </Button>
+      </Controls>
       {/* </ButtonLink> */}
     </Layout>
   );

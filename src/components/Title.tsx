979ae590c856d104e5f26b67e05652cbdc0f9bee
Thomas Constantine Moore <talk@thomas.tech>
2021-07-14 12:26:40 -0400

    one more adjustment

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,6 +5,9 @@ const H1 = styled.h1`
   font-family: AcademyEngravedLetPlain, Academy Engraved LET, Garamond, serif;
   font-weight: 400;
   margin-bottom: 0;
+  @media (max-width: 700px) {
+    margin-bottom: 20px;
+  }
 `;
 
 const Title: React.FC = () => <H1>Thomas Constantine Moore is dead.</H1>;

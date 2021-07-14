e7b1af51a9ee3318ad0048e1ff1323dc8639dbb2
Thomas Constantine Moore <talk@thomas.tech>
2021-07-14 11:16:59 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,14 @@ module.exports = {
         display: 'swap',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-T42M7FBDGX', // Google Analytics / GA
+        ],
+      },
+    },
   ],
 };
